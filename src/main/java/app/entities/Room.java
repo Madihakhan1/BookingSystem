ec8fbe241d25df21b63fac0fc711db294ca950b2
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-12-03 12:38:51 +0100

    Tilføjet atributter og metoder

--- src/main/java/app/entities/Room.java
+++ src/main/java/app/entities/Room.java
@@ -1,4 +1,28 @@
 package app.entities;
 
 public class Room {
+
+    private String room_number;
+    private String description;
+
+    public Room(String room_number, String description) {
+        this.room_number = room_number;
+        this.description = description;
+    }
+
+    public String getRoom_number() {
+        return room_number;
+    }
+
+    public void setRoom_number(String room_number) {
+        this.room_number = room_number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
