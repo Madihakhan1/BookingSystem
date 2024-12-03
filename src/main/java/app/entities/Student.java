88aee1e907bb5d6d79693264dbbe243290f91c6f
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-12-03 12:38:47 +0100

    Tilføjet atributter og metoder

--- src/main/java/app/entities/Student.java
+++ src/main/java/app/entities/Student.java
@@ -2,5 +2,37 @@ package app.entities;
 
 public class Student {
 
-    private String
+    private String email;
+    private String name;
+    private String phone;
+
+    public Student(String email, String name, String phone) {
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
