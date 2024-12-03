2d30803fb9d451375935319a300c191a2066e4f7
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-12-03 12:38:57 +0100

    Tilføjet atributter og metoder

--- src/main/java/app/entities/Booking.java
+++ src/main/java/app/entities/Booking.java
@@ -1,4 +1,50 @@
 package app.entities;
 
+import java.time.LocalDate;
+
 public class Booking {
+
+    private LocalDate booking_date;
+    private int days;
+    private String comment;
+    private String booking_status;
+
+    public Booking(LocalDate booking_date, int days, String comment, String booking_status) {
+        this.booking_date = booking_date;
+        this.days = days;
+        this.comment = comment;
+        this.booking_status = booking_status;
+    }
+
+    public LocalDate getBooking_date() {
+        return booking_date;
+    }
+
+    public void setBooking_date(LocalDate booking_date) {
+        this.booking_date = booking_date;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getBooking_status() {
+        return booking_status;
+    }
+
+    public void setBooking_status(String booking_status) {
+        this.booking_status = booking_status;
+    }
 }
