fc085ff8d921abe60e4046b8e1fe47bb306387f5
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-12-03 12:38:54 +0100

    Tilføjet atributter og metoder

--- src/main/java/app/entities/Item.java
+++ src/main/java/app/entities/Item.java
@@ -1,4 +1,28 @@
 package app.entities;
 
 public class Item {
+
+    private String item_name;
+    private String description;
+
+    public Item(String item_name, String description) {
+        this.item_name = item_name;
+        this.description = description;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
