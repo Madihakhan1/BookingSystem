5fe7d2c255bba109ba24cb6937d1177ca9a19dff
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-12-04 11:09:14 +0100

    ny konstruktor

--- src/main/java/app/entities/Student.java
+++ src/main/java/app/entities/Student.java
@@ -5,11 +5,19 @@ public class Student {
     private String email;
     private String name;
     private String phone;
+    private String password;
 
     public Student(String email, String name, String phone) {
         this.email = email;
         this.name = name;
         this.phone = phone;
+    }
+
+    public Student(String email, String name, String phone, String password){
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        this.password = password;
     }
 
     public String getEmail() {
