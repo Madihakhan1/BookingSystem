a858c5c95a8a63ff17789d8aee3dd0e8bc175f03
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-12-08 19:00:52 +0100

    student opdateret

--- src/main/java/app/entities/Student.java
+++ src/main/java/app/entities/Student.java
@@ -1,46 +1,15 @@
 package app.entities;
 
-public class Student {
-
-    private String email;
-    private String name;
-    private String phone;
-    private String password;
+public class Student extends User {
 
     public Student(String email, String name, String phone) {
-        this.email = email;
-        this.name = name;
-        this.phone = phone;
+        super(email, name, phone);
+        this.isAdmin = false;
     }
 
-    public Student(String email, String name, String phone, String password){
-        this.email = email;
-        this.name = name;
-        this.phone = phone;
-        this.password = password;
+    public Student(String email, String name, String phone, String password) {
+        super(email, name, phone, password);
+        this.isAdmin = false;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
